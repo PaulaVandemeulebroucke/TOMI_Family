44c383864eae73792d01b42d73301973c8049749
PaulaVandemeulebroucke <paula.vandemeulebroucke@gmail.com>
2018-03-17 13:13:14 +0100

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,21 +43,34 @@ Par ailleurs, des écoles de commerce et de gestion ou des organismes spécialis
     career:  "L’acheteur d’espaces publicitaires peut évoluer vers des fonctions de management ou de direction d’un service achat ou d’un groupe achat. L’expérience, les performances et les qualités personnelles déterminent les promotions possibles dans un milieu professionnel où le turn over est important.",
     similar_job:  ["ASSISTANT DE COMMUNICATION / ASSISTANTE DE COMMUNICATION", "PLANNEUR STRATÉGIQUE", "CHEF DE PUBLICITÉ", "CHEF DE PRODUIT", "MÉDIA-PLANNER", "DIRECTEUR / DIRECTRICE DU MARKETING", "RESPONSABLE DE COMMUNICATION", "ATTACHÉ / ATTACHÉE DE PRESSE", "CHARGÉ DE PRODUCTION", "CHARGÉ(E) DE RELATIONS PUBLIQUES"],
     interest:  ["CONVAINCRE, COMMUNIQUER"],
-    statut:        ''
+    statut: ""
   },
   {
-    name:         '',
-    photo:        '',
-    synonym:      '',
-    sector:       [],
-    short_description:  '',
-    description:  '',
-    study:  '',
-    salary:  '',
-    career:  '',
-    similar_job:  [],
-    interest:  [],
-    statut:        ''
+    name:         "AGENT DE STÉRILISATION",
+    photo:        "agent_de_stérilisation",
+    synonym:      "",
+    sector:       ["BIOLOGIE", "CHIMIE", "ENVIRONNEMENT", "NATURE", "NETTOYAGE", "SANTÉ"],
+    short_description: "Récupérer, laver, stériliser, décontaminer, désinfecter tous les instruments et linges utilisés dans les établissements de soins (hôpitaux, cliniques, blocs opératoires, maisons de retraites, de cure) telles sont les missions d’un agent de stérilisation.",
+    description:  "L’agent de stérilisation récupère et trie le matériel à stériliser. Avant la stérilisation elle-même il le lave, le sèche. Il assure le fonctionnement d’un autoclave qui est un appareil à fermeture hermétique pour réaliser sous pression une stérilisation à la vapeur. Il en valide les cycles.
+Il reconstitue les plateaux opératoires après avoir conditionné le matériel médicochirurgical et les pansements. Le pliage et le linge à stériliser est aussi de sa compétence. De même que le nettoyage des locaux. Il livre dans les services le matériel traité.
+Pour ces tâches, l’agent respecte des protocoles opératoires d’hygiène et de sécurité. Il choisit des produits adaptés à chaque traitement en fonction du matériel concerné. Il évalue l’état du matériel de stérilisation et signale les problèmes éventuels.
+L’agent de stérilisation organise son activité en fonction des urgences et représente un lien avec les services de soins et médico-techniques. Il est en relation avec le responsable de l’hygiène et de la sécurité, avec la pharmacie de l’établissement et les sociétés de maintenance des autoclaves.
+Cet agent travaille souvent dans un milieu chaud et humide. En principe, ses horaires sont réguliers mais sont soumis à un roulement en fonction d’un tableau de service ou d’urgences.",
+    study:  "Les établissements de santé recrutent des agents possédant un diplôme de niveau CAP ou bac pro avec des options maintenance et entretien des locaux ou options à caractère sanitaire ou social :
+niveau CAP
+- CAP APH - agent de propreté et d'hygiène,
+- titre agent de stérilisation en milieu hospitalier (GIP FCIP CAFOC, Greta 92).
+niveau Bac
+- Bac pro HPS - hygiène, propreté, stérilisation,
+
+Certains agents de stérilisation sont déjà titulaires du DEAS (diplôme d’Etat d’aide-soignant) ou du BP (brevet professionnnel) de préparateur en pharmacie.
+Dans la fonction publique hospitalière le recrutement s’effectue sur concours des agents de service hospitalier (ASH).",
+    salary:  "Salaire en début de carrière :  1 600 € brut/ mois.",
+    career:  "Une évolution de carrière est possible, si l’agent de stérilisation n’est pas déjà aide-soignant, en passant le DEAS.
+A plus longue échéance, les IFSI proposent des stages de perfectionnement et des formations au métier d’infirmier après 3 années de service. L’aide-soignant peut alors se présenter aux épreuves de sélection à l’entrée des IFSI.",
+    similar_job:  ["AGENT THERMAL", "AIDE-SOIGNANTE / AIDE-SOIGNANT", "BRANCARDIER / BRANCARDIÈRE", "PRÉPARATEUR EN PHARMACIE / PRÉPARATRICE EN PHARMACIE", "INFIRMIÈRE - INFIRMIER", "THANATOPRACTEUR"],
+    interest:  ["PROTÉGER L'ENVIRONNEMENT", "SOIGNER, SECOURIR"],
+    statut: ""
   }
 ]
 Job.create!(jobs)
