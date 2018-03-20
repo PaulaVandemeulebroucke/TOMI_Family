9568b197d714ba289ffcda115a5a655d8e472f90
PaulaVandemeulebroucke <paula.vandemeulebroucke@gmail.com>
2018-03-20 06:19:02 +0100

    boum

--- app/javascript/packs/components/banner.js
+++ app/javascript/packs/components/banner.js
@@ -3,8 +3,8 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
     strings: ["T O M I - Family"],
-    typeSpeed: 50,
-    loop: true
+    typeSpeed: 60,
+    loop: false
   });
 }
 
