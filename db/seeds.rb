70284b50069b82d4bab1472616bb2446f911ed06
PaulaVandemeulebroucke <paula.vandemeulebroucke@gmail.com>
2018-06-20 11:56:22 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,49 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-sectors = [
+jobinterests = [
   {
-    name: "TRANSPORT"
-  },
-  {
-    name: "LOGISTIQUE"
-  },
-  {
-    name: "SOCIAL"
-  },
-  {
-    name: "INDUSTRIE"
+    job: "ANALYSTE FINANCIER",
+    interest: "MANIER LES CHIFFRES"
   }
 ]
-sectors.each do |sector|
-  new_sector = Sector.new(sector)
-  new_sector.save
-end
-
-#########################################
-
-jobsectors = [
-  {
-    job: "AGENT DE CONTRÔLE DES TRANSPORTS",
-    sector: "TRANSPORT"
-  },
-  {
-    job: "AGENT DE CONTRÔLE DES TRANSPORTS",
-    sector: "LOGISTIQUE"
-  },
-  {
-    job: "AGENT D'ENTRETIEN",
-    sector: "SOCIAL"
-  },
-  {
-    job: "AFFÛTEUR / AFFUTEUSE",
-    sector: "INDUSTRIE"
-  }
-]
-jobsectors.each do |jobsector|
-  new_jobsector = JobSector.create(job: Job.find_by_name(jobsector[:job]), sector: Sector.find_by_name(jobsector[:sector]))
-  puts jobsector[:job]
+jobinterests.each do |jobinterest|
+  new_jobinterest = JobInterest.create(job: Job.find_by_name(jobinterest[:job]), interest: Interest.find_by_name(jobinterest[:interest]))
 end
 
 puts 'Finished!'
+
 
