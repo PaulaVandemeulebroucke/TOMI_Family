a0646a5eac4dde8ec91fcd4334b6b0d9d373a904
PaulaVandemeulebroucke <paula.vandemeulebroucke@gmail.com>
2018-06-27 23:44:23 +0200

    ok

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,19 +1,8 @@
 class Job < ApplicationRecord
-  include PgSearch
-  pg_search_scope :search_by_name,
-    against: [ :name ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
   mount_uploader :photo, PhotoUploader
   acts_as_votable
   has_many :job_interests
   has_many :job_sectors
   has_many :interests, through: :job_interests
   has_many :sectors, through: :job_sectors
-  # belongs_to :user
-  include PgSearch
-  pg_search_scope :global_search, {
-    against: [ :name ]
-  }
 end
