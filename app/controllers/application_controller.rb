b22a3a842003283bdd906f40b7dc41001209ec61
PaulaVandemeulebroucke <paula.vandemeulebroucke@gmail.com>
2018-07-26 10:53:45 +0200

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # app/controllers/application_controller.rb
+
+def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_category])
